27d56f867ab104432e0e0573f1288df0f2123549
Macharia777 <gacheruarchibald@gmail.com>
2025-01-14 23:35:00 +0300

    changes to the db

--- custom_national_id/__manifest__.py
+++ custom_national_id/__manifest__.py
@@ -11,7 +11,7 @@
         'security/security.xml',
         'security/ir.model.access.csv',
         'views/national_id_views.xml',
-        # 'data/sequences.xml',
+        'data/sequences.xml',
     ],
     'installable': True,
     'application': True,
